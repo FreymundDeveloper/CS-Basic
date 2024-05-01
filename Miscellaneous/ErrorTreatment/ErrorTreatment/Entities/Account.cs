066c78c34dce3d5bf7ee72036634d916928bccd2
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-04-30 23:02:30 -0300

    Error Treatment Use

---
+++ Miscellaneous/ErrorTreatment/ErrorTreatment/Entities/Account.cs
@@ -0,0 +1,49 @@
+﻿using ErrorTreatment.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorTreatment.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string? Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit) 
+        {
+            Number = number;   
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance < amount)
+            {
+                throw new DomainException("Insufficient balace");
+            }
+            if (amount > WithdrawLimit) 
+            {
+                throw new DomainException("Out of bounds");
+            }
+            Balance -= amount;
+        }
+
+        public string ReturnData()
+        {
+            return $"New balance: {Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Miscellaneous/ErrorTreatment/ErrorTreatment/Program.cs
@@ -0,0 +1,36 @@
+﻿using ErrorTreatment.Entities;
+using ErrorTreatment.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace ErrorTreatment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine()!);
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine()!;
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            Console.Write("Enter amount for withdraw: ");
+
+            try
+            {
+                account.Withdraw(double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture));
+                Console.WriteLine(account.ReturnData());
+            } 
+            catch (DomainException ex) {
+                Console.WriteLine($"Withdraw error: {ex.Message}");            
+            }
+        }
+    }
+}
