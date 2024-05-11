165420ff4e339969427cbc188ea7f0bbcb7f4d98
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-05-11 02:44:02 -0300

    Files Manipulation

---
+++ Miscellaneous/FilesManipulation/FilesTreatment/FilesTreatment/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesTreatment.Entities
+{
+    internal class Product
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string TotalPrice()
+        {
+            return $"{(Price * Quantity).ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Miscellaneous/FilesManipulation/FilesTreatment/FilesTreatment/Program.cs
@@ -0,0 +1,36 @@
+﻿using FilesTreatment.Entities;
+using System;
+using System.Globalization;
+
+namespace FilesTreatment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Remember to remove Summary.csv, if there is one.
+
+                string sourcePath = @"C:\Users\Lucas\Desktop\CS-Basic\Miscellaneous\FilesManipulation\Files";
+                string[] fileLines = File.ReadAllLines(sourcePath + @"\Test.csv");
+
+                using(StreamWriter newFile = File.AppendText(sourcePath + @"\Summary.csv"))
+                {
+                    foreach (string line in fileLines)
+                    {
+                        string[] contentLine = line.Split(',');
+
+                        Product product = new Product(contentLine[0], double.Parse(contentLine[1], CultureInfo.InvariantCulture), int.Parse(contentLine[2]));
+                        newFile.WriteLine($"{product.Name}, {product.TotalPrice()}");
+                    }
+                }
+
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("ERROR Found: \n" + ex.Message);
+            }
+        }
+    }
+}
