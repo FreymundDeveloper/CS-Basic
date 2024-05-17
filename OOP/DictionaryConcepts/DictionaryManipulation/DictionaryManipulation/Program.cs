56b1498486aa28b1a4f448d083ca8fe1fd53fdd4
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-05-17 17:08:45 -0300

    Dictionary Concepts

---
+++ OOP/DictionaryConcepts/DictionaryManipulation/DictionaryManipulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DictionaryManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(@"C:\Users\Lucas\Desktop\CS-Basic\OOP\DictionaryConcepts\Files\in.csv")) 
+            {
+                Dictionary<string, int> map = new Dictionary<string, int>();
+
+                while (!reader.EndOfStream)
+                {
+                    string[] data = reader.ReadLine()!.Split(',');
+
+                    if (map.ContainsKey(data[0])) map[data[0]] += int.Parse(data[1]);
+                    else map[data[0]] = int.Parse(data[1]);
+                }
+
+                foreach (var item in map)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+    }
+}
